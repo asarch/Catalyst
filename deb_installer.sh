04c9ac646c26e9901decc6bda05c462da9c24acb
asarch <asarch@hotmail.com>
2017-03-20 11:44:44 -0600

    Added the one line command.

--- deb_installer.sh
+++ deb_installer.sh
@@ -19,3 +19,6 @@ libcatalyst-view-tt-perl \
 libdatetime-format-pg-perl \
 libcatalyst-action-renderview-perl \
 libdbd-pg-perl
+
+# In one line:
+# aptitude -y install libdbd-pg-perl libcatalyst-manual-perl libcatalyst-perl libhtml-formhandler-model-dbic-perl libcatalyst-plugin-stacktrace-perl libcatalyst-plugin-static-simple-perl libcatalyst-plugin-configloader-perl libdbix-class-schema-loader-perl libcatalyst-devel-perl libterm-size-any-perl libcatalyst-model-dbic-schema-perl libcatalyst-view-tt-perl libdatetime-format-pg-perl libcatalyst-action-renderview-perl
