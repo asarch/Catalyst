ceee45c108ae903da38361c94c96cb06c925eab8
asarch <asarch@hotmail.com>
2021-10-18 21:23:19 -0500

    Added starman

--- deb_installer.sh
+++ deb_installer.sh
@@ -18,7 +18,8 @@ libcatalyst-model-dbic-schema-perl \
 libcatalyst-view-tt-perl \
 libdatetime-format-pg-perl \
 libcatalyst-action-renderview-perl \
-libdbd-pg-perl
+libdbd-pg-perl \
+starman
 
 # In one line:
-# aptitude -y install libdbd-pg-perl libcatalyst-manual-perl libcatalyst-perl libhtml-formhandler-model-dbic-perl libcatalyst-plugin-stacktrace-perl libcatalyst-plugin-static-simple-perl libcatalyst-plugin-configloader-perl libdbix-class-schema-loader-perl libcatalyst-devel-perl libterm-size-any-perl libcatalyst-model-dbic-schema-perl libcatalyst-view-tt-perl libdatetime-format-pg-perl libcatalyst-action-renderview-perl
+# aptitude -y install libdbd-pg-perl libcatalyst-manual-perl libcatalyst-perl libhtml-formhandler-model-dbic-perl libcatalyst-plugin-stacktrace-perl libcatalyst-plugin-static-simple-perl libcatalyst-plugin-configloader-perl libdbix-class-schema-loader-perl libcatalyst-devel-perl libterm-size-any-perl libcatalyst-model-dbic-schema-perl libcatalyst-view-tt-perl libdatetime-format-pg-perl libcatalyst-action-renderview-perl starman
